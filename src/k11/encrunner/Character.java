3627ec19b00ed9658d9581606f323648c4c1469e
Bay Grabowski <baygrabowski@gmail.com>
2011-08-01 14:44:46 -0700

    Added non-functional POST code to Character.class, also used encID from MainMenu.class in Character.class
    Signed-off-by: Bay Grabowski <baygrabowski@gmail.com>

--- src/k11/encrunner/Character.java
+++ src/k11/encrunner/Character.java
@@ -1,10 +1,19 @@
 package k11.encrunner;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.Editable;
-import android.view.LayoutInflater;
-import android.widget.EditText;
+import android.util.Log;
 import android.widget.TextView;
 
 public class Character extends Activity {
@@ -13,10 +22,33 @@ public class Character extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.character);
 		
-		//TextView debugTextView = (TextView)MainMenu.findViewById(R.id.encID_text);
+		//Posting XML Test, should update Baern's HP from 45 to 40 in encounter 65
+		String xml = "<character><name>Kelithz</name></character>";
+		StringEntity se = null;
+		try {
+			se = new StringEntity(xml,"UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		se.setContentType("application/rest+xml");
+		HttpPost postRequest = new HttpPost("http://encrunner.heroku.com/characters/17.xml?api_key=7686f5c264fa75e2db2816ab212ff95c16b2f2cf");
+		postRequest.setEntity(se);
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpResponse response = null;
+		try {
+			response = httpclient.execute(postRequest);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Log.d("HTTPResponse", String.valueOf(response.getStatusLine().getStatusCode()));
 		
-		
-		Encounter encounter  = new Encounter(65);
+		SharedPreferences mPrefs = getSharedPreferences("prefs",0);
+		Encounter encounter  = new Encounter(mPrefs.getInt("encID", 0));
 		TextView debugTextView = (TextView) findViewById(R.id.character_content);
 		debugTextView.setText(String.valueOf(encounter.creatureList.get(0).name));
 	}
--- src/k11/encrunner/MainMenu.java
+++ src/k11/encrunner/MainMenu.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
